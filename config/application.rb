85e57cb94dbe3342bab64215b9fd69a67e3d7343
Giyas Guliyev <g.g.giyas@gmail.com>
2017-10-20 12:35:00 +0300

    Adding CROS and Puma server

--- config/application.rb
+++ config/application.rb
@@ -29,6 +29,18 @@ module Module2Core
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
+    config.middleware.insert_before 0, "Rack::Cors" do
+      allow do
+        # origins 'SiteB.com'
+        origins '*'
+
+        resource '/api/*',
+        :headers => :any,
+        :methods => [:get, :post, :put, :delete, :options]
+      end
+    end
+
+
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
   end
